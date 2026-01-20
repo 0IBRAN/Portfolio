51526a4756d3abdb836e71d6abd4321c14fc7b30
0IBRAN <god2006.28@gmail.com>
2026-01-20 21:56:05 +0530

    Create script.js

---
+++ Project-1/script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(e){
+  e.preventDefault();
+  document.getElementById("success").innerText = "Message sent successfully!";
+});
